a2eb93d76c8c584a1a5a6669a5258a76c331856b
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2024-04-15 21:25:06 +0000

    Code Review 1
    * Change MD5 to EVP_blake2b512 to avoid the chance of a false positive.
    * Print an error message if there were no arguments.
    * Emit a warning if there was an error whilst processing a file.
    * Emit a warning for unreadable files and directories.
    * Change unordered_map to unordered_multimap, since the order of the entries is not significant.
    * Read each file in chunks of 64 KiB instead of a string.
    * Only hexify the hashes of the files that during printing that are duplicates.
    * Use a single digest context for each file.
    * First check the sizes of the files, and only when they are equal, hash the files.
    * Add error handling after reading the file.
    * Use an unordered_set to keep track of processed paths to avoid duplicate paths.
    * Switch to using the other overload of file_size() that doesn't throw an exception.
    * Notify the client that an entry is being skipped for everything that is not a regular file.

--- dedup.cpp
+++ dedup.cpp
@@ -1,3 +1,6 @@
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
@@ -7,37 +10,31 @@
 #include <string_view>
 #include <system_error>
 #include <unordered_map>
-#include <vector>
+#include <unordered_set>
 
 #include <openssl/evp.h>
 
-static std::optional<std::string> compute_file_hash(EVP_MD_CTX *context,
+static std::unordered_set<std::string> processed_paths {};
+
+static std::optional<std::string> compute_file_hash(EVP_MD_CTX* context,
                                                     const std::string& fpath)
 {
     EVP_MD_CTX_reset(context);
     unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int hash_len{0};
+    unsigned int hash_len {0};
+    std::ifstream file {fpath};
 
-    if (EVP_DigestInit_ex(context, EVP_blake2b512(), nullptr) == 0) {
+    if (EVP_DigestInit_ex(context, EVP_blake2b512(), nullptr) == 0 || !file) {
         return std::nullopt;
     }
 
-    std::ifstream file{fpath};
-
-    if (!file) {
-        std::cerr << "error: failed to read: " << fpath << " :"
-                  << std::error_code{errno, std::generic_category()}.message()
-                  << "\n";
-        return std::nullopt;
-    }
-
-    constexpr std::size_t bufsize{65536};
+    constexpr std::size_t bufsize {65536};
     char buf[bufsize];
 
     while (file) {
         file.read(buf, bufsize);
 
-        const std::size_t count{static_cast<std::size_t>(file.gcount())};
+        const std::size_t count {static_cast<std::size_t>(file.gcount())};
 
         if (count == 0) {
             break;
@@ -48,48 +45,85 @@ static std::optional<std::string> compute_file_hash(EVP_MD_CTX *context,
         }
     }
 
-    if (EVP_DigestFinal_ex(context, hash, &hash_len) == 0) {
+    if (file.bad() || EVP_DigestFinal_ex(context, hash, &hash_len) == 0) {
         return std::nullopt;
     }
 
-    return std::string{reinterpret_cast<char *>(hash), hash_len};
+    return std::string {reinterpret_cast<char*>(hash), hash_len};
 }
 
-static bool find_duplicates(std::unordered_multimap<std::string, std::string>& file_hashes,
-                            const std::string_view& dir)
+static void populate_size_map(
+    std::unordered_multimap<std::uintmax_t, std::string>& size_map,
+    const std::string_view& dir)
 {
-    EVP_MD_CTX *const context{EVP_MD_CTX_new()};
+    for (const auto& entry :
+         std::filesystem::recursive_directory_iterator(dir)) {
+        if (processed_paths.find(entry.path().string()) !=
+            processed_paths.end()) {
+            continue;
+        }
+        processed_paths.insert(entry.path().string());
+
+        if (entry.is_regular_file()) {
+            std::error_code ecode {};
+            const std::uintmax_t fsize =
+                std::filesystem::file_size(entry.path(), ecode);
+
+            if (ecode) {
+                std::cerr << "error: failed to process "
+                          << std::quoted(entry.path().string())
+                          << std::error_code {errno, std::generic_category()}
+                                 .message()
+                          << "\n";
+            }
+            else {
+                size_map.emplace(fsize, entry.path().string());
+            }
+        }
+        else {
+            std::cerr << "Skipping entry: "
+                      << std::quoted(entry.path().string()) << "\n";
+        }
+    }
+}
+
+static bool populate_hash_map(
+    const std::unordered_multimap<std::uintmax_t, std::string>& size_map,
+    std::unordered_multimap<std::string, std::string>& hash_map)
+{
+    EVP_MD_CTX* const context {EVP_MD_CTX_new()};
 
     if (context == nullptr) {
         std::cerr << "error: "
-                  << std::error_code{errno, std::generic_category()}.message()
+                  << std::error_code {errno, std::generic_category()}.message()
                   << "\n";
         return false;
     }
 
-    for (const auto &entry :
-         std::filesystem::recursive_directory_iterator(dir)) {
-        std::string entry_path{entry.path().string()};
+    for (auto it {size_map.begin()}; it != size_map.end(); ++it) {
+        const auto res {size_map.equal_range(it->first)};
 
-        if (entry.is_regular_file() && !entry.is_symlink()) {
-            const auto res{compute_file_hash(context, entry_path)};
+        if (std::distance(res.first, res.second) > 1) {
+            const auto hash {compute_file_hash(context, it->second)};
 
-            if (res != std::nullopt) {
-                file_hashes.emplace(res.value(), entry_path);
-            } else {
-                std::cerr << "error: failed to process file: " << entry_path
-                          << "\n";
+            if (hash != std::nullopt) {
+                hash_map.emplace(hash.value(), it->second);
             }
-        }     
+            else {
+                std::cerr << "error: failed to process "
+                          << std::quoted(it->second) << "\n";
+            }
+        }
     }
+
     EVP_MD_CTX_free(context);
     return true;
 }
 
 static std::string str_to_hex(const std::string& str)
 {
-    std::basic_string<unsigned char> s{str.begin(), str.end()};
-    std::stringstream ss{};
+    std::basic_string<unsigned char> s {str.begin(), str.end()};
+    std::stringstream ss {};
 
     for (unsigned int i = 0; i < s.size(); ++i) {
         ss << std::hex << std::setw(2) << std::setfill('0')
@@ -99,31 +133,31 @@ static std::string str_to_hex(const std::string& str)
     return ss.str();
 }
 
-static void print_duplicates(std::unordered_multimap<std::string, std::string>& file_hashes)
+static void
+print_duplicates(std::unordered_multimap<std::string, std::string>& hash_map)
 {
-    std::string curr_key{""};
+    std::string curr_key {""};
 
-    for (auto it{file_hashes.begin()}; it != file_hashes.end(); ++it) {
+    for (auto it {hash_map.begin()}; it != hash_map.end(); ++it) {
         if (curr_key == it->first) {
             continue;
         }
         else {
             curr_key = it->first;
         }
-
-        const auto res{file_hashes.equal_range(it->first)};
+        const auto res {hash_map.equal_range(it->first)};
 
         if (std::distance(res.first, res.second) > 1) {
             std::cout << str_to_hex(it->first) << "\n";
 
-            for (auto it{res.first}; it != res.second; ++it) {
+            for (auto it {res.first}; it != res.second; ++it) {
                 std::cout << "\t" << it->second << "\n";
             }
         }
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argc < 2) {
         std::cerr << "Error: no argument provided.\n"
@@ -132,12 +166,16 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    std::unordered_multimap<std::string, std::string> file_hashes;
+    std::unordered_multimap<std::uintmax_t, std::string> size_map {};
+    std::unordered_multimap<std::string, std::string> hash_map {};
 
-    for (int i{1}; i < argc; ++i) {
-        if (!find_duplicates(file_hashes, argv[i])) {
+    for (int i {1}; i < argc; ++i) {
+        populate_size_map(size_map, argv[i]);
+
+        if (!populate_hash_map(size_map, hash_map)) {
             return EXIT_FAILURE;
         }
     }
-    print_duplicates(file_hashes);
+
+    print_duplicates(hash_map);
 }
